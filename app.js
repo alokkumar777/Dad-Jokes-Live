03b6db9ecc6efba949867a265f5fa925b8c961f4
Alok Kumar <136910791+alokkumar777@users.noreply.github.com>
2025-06-15 14:46:11 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+// fetch-jokes.js
+import {
+  getJokeButton,
+  jokeDisplay,
+  fetchRandomJoke,
+} from "./APIs/fetch-jokes.js";
+
+// search-jokes.js
+import {
+  searchButton,
+  searchTermInput,
+  searchJokes,
+} from "./APIs/search-jokes.js";
