4bfb9370532066c7031338048a39048fc6b3f225
Alok Kumar <136910791+alokkumar777@users.noreply.github.com>
2025-06-15 14:51:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ import {
   getJokeButton,
   jokeDisplay,
   fetchRandomJoke,
-} from "./APIs/fetch-jokes.js";
+} from "./API/fetch-jokes.js";
 
 // search-jokes.js
 import {
   searchButton,
   searchTermInput,
   searchJokes,
-} from "./APIs/search-jokes.js";
+} from "./API/search-jokes.js";
